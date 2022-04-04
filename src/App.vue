c1bd2ef00813a92521768272319c379f98752938
jaylen.chan <jaylen.chan@hotmail.com>
2022-04-04 10:58:09 +0800

    feat: init vue-router

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'jaylen'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
