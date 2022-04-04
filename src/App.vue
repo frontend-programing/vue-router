1fc5b02365b881c7f7648c9213332ab2e8cf8cf6
jaylen.chan <jaylen.chan@hotmail.com>
2022-04-04 18:01:31 +0800

    feat: route匹配

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    {{ name }}
+    <router-link>首页</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'App',
   data() {
     return {
       name: 'jaylen'
